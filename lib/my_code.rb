899fe88ba3c497c9e69ffe0abab2769162d4da46
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-02 03:25:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,35 @@
-# Your Code Here
+  
+
+def map(source_array)
+  new_array = []
+  
+  source_array.each do |element|
+    new_array.push(yield(element))
+  end
+  
+  new_array
+end
+
+def reduce(source_array, starting_value = 0)
+  total = starting_value
+  new_array = []
+ 
+  
+  source_array.each do |element|
+    if element
+      #total += element
+      puts "total: #{total} \nelem: #{elem}"
+      total += yield(total, element)
+    else
+      if !element 
+        return false
+      end
+    end
+  end
+  
+  #yield(total, starting_value)
+  #new_array
+  total
+end
+
+reduce()
